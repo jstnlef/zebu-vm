66a09287238c303b1374331226d5249b25aa9661
qinsoon <qinsoon@gmail.com>
2016-04-11 15:04:44 +1000

    [wip] started a bit on inst selection. will work on GC for a while

--- src/compiler/backend/x86_64/inst_sel.rs
+++ src/compiler/backend/x86_64/inst_sel.rs
@@ -1,4 +1,5 @@
 use ast::ir::*;
+use ast::ptr::*;
 use ast::inst::Instruction_::*;
 use vm::context::VMContext;
 
@@ -21,6 +22,49 @@ impl CompilerPass for InstructionSelection {
     
     #[allow(unused_variables)]
     fn start_function(&mut self, vm_context: &VMContext, func: &mut MuFunction) {
-        println!("{}", self.name());
+        debug!("{}", self.name());
+    }
+    
+    fn visit_function(&mut self, vm_context: &VMContext, func: &mut MuFunction) {
+        for block_label in func.block_trace.as_ref().unwrap() {
+            let block = func.content.as_mut().unwrap().get_block_mut(block_label);
+            
+            let block_content = block.content.as_mut().unwrap();
+            
+            for inst in block_content.body.iter_mut() {
+                instruction_select(inst);
+            }
+        }
     }
 }
+
+fn instruction_select(inst: &mut P<TreeNode>) {
+    match inst.v {
+        TreeNode_::Instruction(ref inst) => {
+            match inst.v {
+                Branch2{cond, ref true_dest, ref false_dest, true_prob} => {
+                    let ref cond = inst.ops.borrow()[cond];
+                    
+                    match cond.v {
+                        TreeNode_::Instruction(ref inst) => {
+                            match inst.v {
+                                CmpOp(op, op1, op2) => {
+                                    
+                                }, 
+                                
+                                _ => panic!("unexpected inst as child node of branch2: {}", cond)
+                            }
+                        },
+                        
+                        TreeNode_::Value(ref pv) => {
+                            
+                        }
+                    }
+                },
+                
+                _ => unimplemented!()
+            }
+        },
+        _ => panic!("expected instruction")
+    }
+} 
