9abaefcab0631f8b3cae5f98500877258c24a85e
John Zhang <john.zhang@anu.edu.au>
2016-10-20 11:17:43 +1100

    header file for rust implementation specific functions (e.g. vm creation function)

---
+++ src/vm/api/mu-impl-fast.h
@@ -0,0 +1,16 @@
+#ifndef __MU_IMPL_FAST_H__
+#define __MU_IMPL_FAST_H__
+
+#include "muapi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+MuVM *mu_fastimpl_new();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MU_IMPL_FAST_H__
