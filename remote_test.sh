f7074700c35c257261c806755f12e3f8128d933d
qinsoon <qinsoon@gmail.com>
2016-11-25 14:20:28 +1100

    link against -lm for primordial

--- remote_test.sh
+++ remote_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "rsync source to machine: $1..."
-rsync -avz --exclude 'Cargo.lock' --exclude '*.log' --exclude 'emit*' --exclude 'target*' --exclude '.git' --exclude '*__pycache__*'. $1:~/mu/
+rsync -avz --exclude 'Cargo.lock' --exclude '*.log' --exclude 'emit*' --exclude 'target*' --exclude '.git' --exclude '*__pycache__*' . $1:~/mu/
 
 echo "executing tests..."
 ssh $1 "cd ~/mu; ./run_test.sh $2"
