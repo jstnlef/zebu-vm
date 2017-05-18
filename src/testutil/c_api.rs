b76c216c19e6daee06df4b148b282a35ef73d99e
qinsoon <qinsoon@gmail.com>
2017-05-18 14:28:25 +1000

    compile_run_c_test needs to be aware of which build to use

--- src/testutil/c_api.rs
+++ src/testutil/c_api.rs
@@ -6,6 +6,8 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
+use std::env;
+
 pub fn compile_run_c_test(test_file_path: &'static str) -> PathBuf {
     let mut src = Path::new("tests/test_jit").to_path_buf();
     src.push(test_file_path);
@@ -25,7 +27,13 @@ pub fn compile_run_c_test(test_file_path: &'static str) -> PathBuf {
     let mut cc = Command::new(get_test_clang_path());
     cc.arg("-std=c99");
     cc.arg("-Isrc/vm/api");
-    cc.arg("-Ltarget/debug");
+
+    let build = match env::var("ZEBU_BUILD") {
+        Ok(val) => val,
+        Err(_) => "debug".to_string()
+    };
+    cc.arg(format!("-Ltarget/{}", build));
+
     cc.arg("-lmu");
     // src
     cc.arg(src.as_os_str());
