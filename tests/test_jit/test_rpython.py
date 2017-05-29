73d50b591cecdb43d0c7c039893791e9911ea810
qinsoon <qinsoon@gmail.com>
2017-05-29 17:12:01 +1000

    separate SOM test in CI script

--- tests/test_jit/test_rpython.py
+++ tests/test_jit/test_rpython.py
@@ -1406,63 +1406,6 @@ def test_float():
     assert res.out == '(0.893876, 1.000000, 0.447179)\n'
 
 @may_spawn_proc
-def test_RPySOM():
-    from som.vm.universe import main, Exit
-
-    def entry_point(argv):
-        try:
-            main(argv)
-        except Exit, e:
-            return e.code
-        except Exception, e:
-            os.write(2, "ERROR: %s thrown during execution.\n" % e)
-            return 1
-        return 1
-
-    RPYSOM = os.environ.get('RPYSOM', str(py.path.local(__file__).join('..', 'RPySOM')))
-
-    res = run_boot_image(entry_point, '/tmp/RPySOM-no-jit-mu',
-                         args=['-cp', '%(RPYSOM)s/Smalltalk' % locals(),
-                               '%(RPYSOM)s/TestSuite/TestHarness.som' % locals()])
-    assert res.returncode == 0, res.err
-    expected_out = \
-"""\
-Testing...
-Running test EmptyTest
-Running test SystemTest
-Running test ArrayTest
-Running test ClassLoadingTest
-Running test ClosureTest
-Running test CoercionTest
-Running test CompilerReturnTest
-Running test DoubleTest
-Running test HashTest
-Running test IntegerTest
-Warning: Test instance of IntegerTest failed: Identity failed. Expected: true, but Actual: false
-Warning: Test instance of IntegerTest failed: Identity failed. Expected: true, but Actual: false
-Running test ObjectSizeTest
-Warning: Test instance of ObjectSizeTest failed: Plain object does not have size 1.
-Warning: Test instance of ObjectSizeTest failed: Integer object does not have size 1.
-Warning: Test instance of ObjectSizeTest failed: hello String object does not have size 1.
-Warning: Test instance of ObjectSizeTest failed: Empty array object does not have size 1.
-Warning: Test instance of ObjectSizeTest failed: Array object (length 4) does not have size 5.
-Running test PreliminaryTest
-Running test ReflectionTest
-Running test SelfBlockTest
-Running test SuperTest
-Running test SymbolTest
-Running test VectorTest
-Running test BlockTest
-Running test StringTest
-Running test ClassStructureTest
-Definition of Class changed. Testcase needs to be updated.
-Running test DoesNotUnderstandTest
-...done
-"""
-    assert res.out == expected_out
-
-
-@may_spawn_proc
 def test_open_file_as_stream():
     from rpython.rlib.streamio import open_file_as_stream
 
