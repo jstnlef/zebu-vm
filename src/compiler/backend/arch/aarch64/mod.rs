a0f4cd2f05a9ae5a52002b0a8fd28d235092327d
Isaac Oscar Gariano <u6331211@anu.edu.au>
2017-06-16 18:01:59 +1000

    Added license headers

--- src/compiler/backend/arch/aarch64/mod.rs
+++ src/compiler/backend/arch/aarch64/mod.rs
@@ -1,3 +1,17 @@
+// Copyright 2017 The Australian National University
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // TODO: CHECK THAT THE TYPE OF EVERY MEMORY LOCATION HAS THE CORRECT SIZE
 // (the size should be size of the area in memory that it is referring to, and will indicate
 // how much data any load/store instructions that uses it will operate on
