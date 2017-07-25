2852ac057ba4a4b1d3dc33dee74c19a4ae8172b8
Javad Amiri <javad.amiri@anu.edu.au>
2017-07-25 13:42:02 +1000

    Working test groups: binop, floating_point, alloc

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(cfg_target_vendor)]
-
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
