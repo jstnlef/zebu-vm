ed8036413e892681f711c4656c7813c410d7a94b
Isaac Gariano <isaac.oscar.gariano@anu.edu.au>
2017-10-31 10:22:12 +1100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -13,15 +13,12 @@
 # limitations under the License.
 
 set -e
-if [ -z "$MU_ZEBU" ]
-then
-	export MU_ZEBU=$PWD
-fi
+export MU_ZEBU=$(dirname $0)
 export MU_LOG_LEVEL=none
 export RUST_TEST_THREADS=1
 export RUST_BACKTRACE=0
 export PYTHONPATH="$MU_ZEBU/tests/test_jit/mu-client-pypy/:$MU_ZEBU/tests/test_jit/RPySOM/src"
-export LD_LIBRARY_PATH="$MU_ZEBU/target/$ZEBU_BUILD:$MU_ZEBU/tests/test_jit/:$MU_ZEBU/tests/test_jit"
+export LD_LIBRARY_PATH="$MU_ZEBU/target/$ZEBU_BUILD:$MU_ZEBU/tests/test_jit/:$MU_ZEBU/tests/test_jit/emit/:$LD_LIBRARY_PATH"
 export ZEBU_BUILD=release
 
 rm -rf $MU_ZEBU/emit
@@ -56,4 +53,7 @@ else
         git -C ./RPySOM submodule update
 fi
 shopt -s extglob
-pytest ./test_!(pypy).py -v --color=yes 2>&1 | tee $MU_ZEBU/pytest_out.txt
+pytest ./test_!(pypy).py -v --color=yes 2>&1 | tee $MU_ZEBU/pytest_jit_out.txt
+
+cd $MU_ZEBU/tests/test_muc
+pytest ./test_*.py -v --color=yes 2>&1 | tee $MU_ZEBU/pytest_muc_out.txt
